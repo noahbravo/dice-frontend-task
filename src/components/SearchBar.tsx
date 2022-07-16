02943fb598c30a892fae68d6e3e0c1f3ab2cfeb2
Elena <hey@elenabravo.com>
2022-07-17 01:21:22 +0200

    add useDebounce hook

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,38 +1,44 @@
-import React, { useState } from 'react'
-import tw, { styled } from 'twin.macro'
-import { toRem } from '../styles'
+import React, { useState, useEffect, useRef } from 'react'
+import 'twin.macro'
+import { useDebounce } from '../hooks'
 import { SearchIcon } from './ui/svg'
 
 interface SearchBarProps {
   handleSearch: (venue: string) => void
 }
 
-const StyledForm = styled.form`
-  ${tw`w-full relative pb-10 bg-bg-color`}
-  max-width: ${toRem(1144)};
-`
-
-const StyledInput = styled.input`
-  ${tw`w-full rounded-3xl py-3 pr-2 pl-12 text-font-base`};
-`
-
 const SearchBar = ({ handleSearch }: SearchBarProps) => {
   const [value, setValue] = useState('')
+  const debouncedValue = useDebounce<string>(value)
+  const debounceRef = useRef(false)
 
   const onSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
     const { value: searchValue } = event.target
     setValue(searchValue)
-    handleSearch(searchValue)
+    debounceRef.current = true
   }
 
+  useEffect(() => {
+    if (debounceRef.current) {
+      debounceRef.current = false
+      handleSearch(debouncedValue)
+    }
+  }, [debouncedValue, handleSearch])
+
   return (
-    <div tw="flex justify-center w-full bg-bg-color fixed">
-      <StyledForm>
+    <div tw="flex justify-center w-full px-5 pt-1 bg-bg-color fixed left-0 z-10">
+      <form tw="w-full relative pb-10 bg-bg-color max-w-screen-2xl">
         <div tw="absolute top-3 left-4">
           <SearchIcon />
         </div>
-        <StyledInput type="text" value={value} onChange={onSearch} placeholder="Find an event" />
-      </StyledForm>
+        <input
+          tw="w-full rounded-3xl py-3 pr-2 pl-12 text-font-base"
+          type="text"
+          value={value}
+          onChange={onSearch}
+          placeholder="Find an event"
+        />
+      </form>
     </div>
   )
 }
